99cc45baa7454f29bed15e68dd5cbe75a855c98b
Amit Elfassi <amitelfassi@gmail.com>
2025-01-10 19:23:38 +0200

    e

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const tabContents = document.querySelectorAll('[data-tab-content]')
 tabs.forEach(tab =>{
     tab.addEventListener('click',() =>{
         const target = document.querySelector(tab.dataset.tabTarget)
-        tabContents.forEach(tabContents=> tabContents.classList.remove)
-        tabs.forEach(tab=> tab.classList.remove)
+        tabContents.forEach(tabContent => tabContent.classList.remove('active'))
+        tabs.forEach(tab=> tab.classList.remove('active'))
         tab.classList.add('active')
         target.classList.add('active')
     })
